239cfe46054b781b7c7ce3d3ebe4e33d43e4d4b5
FaMuLan <fa_mulan@163.com>
2021-12-20 11:09:30 +0800

    掉线自动重连（未测试）

--- bilibili_inspect_user.py
+++ bilibili_inspect_user.py
@@ -1,4 +1,4 @@
-from time import sleep
+import time
 import websocket
 from urllib.request import urlopen
 import threading
@@ -12,10 +12,17 @@ setting_json = json.loads(setting_file.read())
 #用户配置文件设置
 ws_client = websocket.create_connection("ws://broadcastlv.chat.bilibili.com:2244/sub")
 is_quit = False
+heartbeat_timer = time.time()
+
+def send_enter_pack():
+	enter_pack_post = json.dumps({"roomid": setting_json["roomid"], "clientver": "1.5.10.1", "type": 2, "platform": "web"})
+	enter_pack_header = (len(enter_pack_post) + 16).to_bytes(4, byteorder="big") + (16).to_bytes(2, byteorder="big") + (0).to_bytes(2, byteorder="big") + (7).to_bytes(4, byteorder="big") + (1).to_bytes(4, byteorder="big")
+	enter_pack = enter_pack_header + enter_pack_post.encode("utf-8")
+	ws_client.send(enter_pack)
 
 def inspect_user_following(uid):
 	follow = []
-	page = 1;
+	page = 1
 	while True:
 		follow_url = urlopen("https://api.bilibili.com/x/relation/followings?vmid={0}&pn={1}".format(uid, page))
 		follow_text = follow_url.read()
@@ -38,17 +45,17 @@ def inspect_user_rank(uid):
 	rank = user_info_json["data"]["level"]
 	return rank
 
-def send_enter_pack():
-	enter_pack_post = json.dumps({"roomid": setting_json["roomid"], "clientver": "1.5.10.1", "type": 2, "platform": "web"})
-	enter_pack_header = (len(enter_pack_post) + 16).to_bytes(4, byteorder="big") + (16).to_bytes(2, byteorder="big") + (0).to_bytes(2, byteorder="big") + (7).to_bytes(4, byteorder="big") + (1).to_bytes(4, byteorder="big")
-	enter_pack = enter_pack_header + enter_pack_post.encode("utf-8")
-	ws_client.send(enter_pack)
-
 def send_heartbeat_pack():
+	global heartbeat_timer
 	while not is_quit:
-		heartbeat_pack_header = (16).to_bytes(4, byteorder="big") + (16).to_bytes(2, byteorder="big") + (0).to_bytes(2, byteorder="big") + (2).to_bytes(4, byteorder="big") + (1).to_bytes(4, byteorder="big")
-		ws_client.send(heartbeat_pack_header)
-		sleep(30)
+		if time.time() - heartbeat_timer <= 70.0:
+			heartbeat_pack_header = (16).to_bytes(4, byteorder="big") + (16).to_bytes(2, byteorder="big") + (0).to_bytes(2, byteorder="big") + (2).to_bytes(4, byteorder="big") + (1).to_bytes(4, byteorder="big")
+			ws_client.send(heartbeat_pack_header)
+			heartbeat_timer = time.time()
+		else:
+			send_enter_pack()
+			print("掉线，重连中")
+		time.sleep(30)
 
 def get_text(pack):
 	operation = int.from_bytes(pack[8:12], "big")
@@ -74,50 +81,55 @@ def split_pack(pack):
 	return pack_total
 
 def receive_pack():
+	global heartbeat_timer
 	database = sqlite3.connect("flagged.db")
 	cursor = database.cursor()
 	while not is_quit:
 		recv_pack = ws_client.recv()
 		recv_pack = split_pack(recv_pack)
 		for i in recv_pack:
-			recv_text = get_text(i)
-			if recv_text:
-				recv = json.loads(recv_text)
-				if recv["cmd"] == "INTERACT_WORD":
-					uid = recv["data"]["uid"]
-					uname = recv["data"]["uname"]
-					time = recv["data"]["timestamp"]
-					print("uid: ", uid, "name: ", uname)
-					cursor.execute("select uname from user where uid = {0}".format(uid))
-					#查询是否为已标记用户
-					if not cursor.fetchone():
-						follow = inspect_user_following(uid)
-						flagged = False
-						for i in setting_json["inspect_following"]:
-							if follow.count(i["uid"]):
-								print(i["notification"])
-								flagged = True
-						if flagged:
+			operation = int.from_bytes(i[8:12], "big")
+			if operation == 5:
+				recv_text = i[16:].decode("UTF-8")
+				if recv_text:
+					recv = json.loads(recv_text)
+					if recv["cmd"] == "INTERACT_WORD":
+						uid = recv["data"]["uid"]
+						uname = recv["data"]["uname"]
+						timestamp = recv["data"]["timestamp"]
+						print("uid: ", uid, "name: ", uname)
+						cursor.execute("select uname from user where uid = {0}".format(uid))
+						#查询是否为已标记用户
+						if not cursor.fetchone():
+							follow = inspect_user_following(uid)
+							flagged = False
+							for i in setting_json["inspect_following"]:
+								if follow.count(i["uid"]):
+									print(i["notification"])
+									flagged = True
+							if flagged:
+								rank = inspect_user_rank(uid)
+								cursor.execute("insert into user(uid, uname) values({0}, '{1}')".format(uid, uname))
+								cursor.execute("insert into enter(time, rank, uid) values({0}, {1}, {2})".format(timestamp, rank, uid))
+								database.commit()
+						else:
 							rank = inspect_user_rank(uid)
-							cursor.execute("insert into user(uid, uname) values({0}, '{1}')".format(uid, uname))
-							cursor.execute("insert into enter(time, rank, uid) values({0}, {1}, {2})".format(time, rank, uid))
+							cursor.execute("insert into enter(time, rank, uid) values({0}, {1}, {2})".format(timestamp, uid))
 							database.commit()
-					else:
-						rank = inspect_user_rank(uid)
-						cursor.execute("insert into enter(time, rank, uid) values({0}, {1}, {2})".format(time, uid))
-						database.commit()
-				#想办法标记用户
-				if recv["cmd"] == "DANMU_MSG":
-					uid = recv["info"][2][0]
-					name = recv["info"][2][1]
-					time = recv["info"][0][3]
-					text = recv["info"][1]
-					cursor.execute("select uname from user where uid = {0}".format(uid))
-					if cursor.fetchone():
-						print("{0}: {1}".format(name, text))
-						cursor.execute("insert into message (time, text, uid) values({0}, '{1}', {2})".format(time, text, uid))
-						database.commit()
-				#按照委托要求，我需要连带标记用户的发言也一并记录下来，不一定能当作证据，但可以让被标记用户崩防.jpg
+					#想办法标记用户
+					if recv["cmd"] == "DANMU_MSG":
+						uid = recv["info"][2][0]
+						name = recv["info"][2][1]
+						timestamp = recv["info"][0][3]
+						text = recv["info"][1]
+						cursor.execute("select uname from user where uid = {0}".format(uid))
+						if cursor.fetchone():
+							print("{0}: {1}".format(name, text))
+							cursor.execute("insert into message (time, text, uid) values({0}, '{1}', {2})".format(timestamp, text, uid))
+							database.commit()
+					#按照委托要求，我需要连带标记用户的发言也一并记录下来，不一定能当作证据，但可以让被标记用户崩防.jpg
+			elif operation == 3:
+				heartbeat_timer = time.time()
 
 send_enter_pack()
 #发个进房包
